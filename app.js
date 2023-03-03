0039d9993792415e24e97e70d8b1d57653a9cc05
omerfarukbakir <omerfarukbakir@gmail.com>
2023-03-03 21:20:03 +0300

    Modal and Dynamic Content

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const path = require("path")
 const ejs = require("ejs")
+const Photo = require("./models/Photo")
 
 
 const app = express()
 const port = 3000
+
+//connet DB
+
+mongoose.connect("mongodb://localhost/pcat-test-db")
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs")
 
 //MIDDİLEWARES
 app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
 //ROUTES
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/", async (req, res) => {
+    const photos = await Photo.find({})
+    res.render("index", {
+        photos
+    })
 })
 app.get("/about", (req, res) => {
     res.render("about")
 })
 app.get("/add", (req, res) => {
     res.render("add")
+})
+app.post("/photos", async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect("/")
 })
 
 app.listen(port, () => {
